0435ed36ab7c270b5023c5ee060b0d18689e34a6
Maiki <info@maikpeters.org>
2022-08-25 11:28:17 +0200

    feat: Loginpage
    Loginpage und LoginModel erstellt

---
+++ WebApp/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Account;
+
+public class LoginModel : PageModel
+{
+    [BindProperty] public Credential Credential { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public void OnPost()
+    {
+    }
+}
+
+public class Credential
+{
+    [Required]
+    [Display(Name = "Benutzername: ")]
+    public string UserName { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Passwort: ")]
+    public string Password { get; set; }
+}
